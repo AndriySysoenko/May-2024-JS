e9979d2bfd1ea8db29ca00e99e7e1e9fcf07335b
AndriySysoenko <gaskonets@gmail.com>
2024-08-30 22:17:54 +0300

    creat part hw10 hw10.html

--- HW_10/main.js
+++ HW_10/main.js
@@ -59,3 +59,18 @@ let totalConvert = document.getElementById('total');
 enterKilo.oninput = function(){
     totalConvert.innerText = this.value * 2.2;
 }
+
+// _________________функцію, які дістає потрібний вам масив з localStorage та додає в нього об'єкт__________________//
+let arrOne = [
+    {id:1, name:'slavik', surname:'kovalenko', email:'bviuhil@gmail.com', phone:'+380752818356'},
+    {id:2, name:'viktor', surname:'khristenko', email:'nkhkli@gmail.com', phone:'+380723344555'}
+];
+let arrTwo = [
+    {id:1, name:'seva', surname:'ivanov', email:'gfdghv@gmail.com', phone:'+380727687987'},
+    {id:2, name:'vlad', surname:'sidorov', email:'zdfasedf@gmail.com', phone:'+380754151621'}
+];
+let arrThree = [
+    {id:1, name:'ivan', surname:'kotov', email:'dfsbdthde@gmail.com', phone:'+380756775578'},
+    {id:2, name:'viktoria', surname:'gukova', email:'tdgndthdv@gmail.com', phone:'+380956164589'}
+];
+
